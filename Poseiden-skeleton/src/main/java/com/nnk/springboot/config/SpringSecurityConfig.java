742de078dcd412b5b8980edcf20b391405657acc
adeline-benhaim <adelinebenhaim@gmail.com>
2021-08-26 09:56:23 +0200

    7.2 : Provide hashing of the password

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/config/SpringSecurityConfig.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/SpringSecurityConfig.java
@@ -1,9 +1,12 @@
 package com.nnk.springboot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -26,4 +29,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().exceptionHandling()
                 .accessDeniedPage("/app/error");
     }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
